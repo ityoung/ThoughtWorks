6846bcd731137dcfcb4a2cf43cad35253eaf79d1
ityoung <ityoung@126.com>
2016-09-21 19:25:40 +0800

    Update animal_test.py
    针对animal.py的修改，修改测试。

--- animal_test.py
+++ animal_test.py
@@ -5,7 +5,6 @@ ThoughtWorks' homework: 2017-chengdu
 == Unittest Part ==
 @author: young
 '''
-
 import unittest
 import animal
 
@@ -39,24 +38,20 @@ class TestDict(unittest.TestCase):
         self.assertFalse(animal.isAnimal(animal5DigitPos))
 
     def test_isAnimalDataRight(self):
-        newAnimalCorrect  = 'cat1 10 9'
-        oldAnimalCorrect1 = 'cat1 10 9 2 -1'
-        oldAnimalCorrect2 = 'cat1 12 8 3 4'
-        oldAnimalFalse    = 'cat1 11 8 3 4'
-        newAnimalFalse    = 'cat2 11 8 3 4'
+        animal.lastDateTime = "2016/09/02 22:30:46"
+        newAnimalCorrect = 'cat1 10 9'
         self.assertTrue(animal.isAnimalDataRight(newAnimalCorrect))
+        oldAnimalCorrect1 = 'cat1 10 9 2 -1'
         self.assertTrue(animal.isAnimalDataRight(oldAnimalCorrect1))
+        oldAnimalCorrect2 = 'cat1 12 8 3 4'
         self.assertTrue(animal.isAnimalDataRight(oldAnimalCorrect2))
+        oldAnimalFalse = 'cat1 11 8 3 4'
         self.assertFalse(animal.isAnimalDataRight(oldAnimalFalse))
+        newAnimalFalse = 'cat2 11 8 3 4'
         self.assertFalse(animal.isAnimalDataRight(newAnimalFalse))
 
-    def test_delFalseAnimal(self):
-        animal.animalDict = {'cat2': [2, 3, True], 'cat1': [10, 9, False]}
-        animal.animalDict = animal.delFalseAnimal(animal.animalDict)
-        self.assertIn('cat2', animal.animalDict)
-        self.assertNotIn('cat1', animal.animalDict)
-        
     def test_isDateOrderRight(self):
+        animal.lastDateTime = ''
         timeS = '2016/09/02 22:30:46'
         timeM = '2016/09/02 22:30:52'
         self.assertTrue(animal.isDateOrderRight(timeS))
